76211b2801232483d4b7834db5156e8f6a26aebb
timokluser-dev <timo@timokluser.ch>
2021-12-22 11:45:28 +0100

    add fixtures add fixtures for keyword add fixtures for categories add fixtures for posts

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -12,6 +12,10 @@ if [ "$1" = '/usr/libexec/s2i/run' ] || [ "$3" = '/usr/libexec/s2i/run' ] || [ "
     echo "Load fixtures"
     # insert superuser to db
     python ./manage.py loaddata app/fixtures/users.json
+    python ./manage.py loaddata app/fixtures/keywords.json
+    python ./manage.py loaddata app/fixtures/categories.json
+    python ./manage.py loaddata app/fixtures/posts.json
+
 
     echo "Collect static files"
     python ./manage.py collectstatic --noinput
