cf77b1f69d2ef1a3edda78ca7728026c9c997724
timokluser-dev <timo@timokluser.ch>
2022-01-06 11:23:01 +0100

    add delete token endpoint

--- api/mutations.py
+++ api/mutations.py
@@ -24,3 +24,4 @@ class JwtMutation(graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
+    delete_token = graphql_jwt.DeleteJSONWebTokenCookie.Field()
